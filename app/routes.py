bd1b95858d99337e2ab2055cdf2bce2389625f0f
6dsht5 <115688624+6dsht5@users.noreply.github.com>
2023-03-10 19:09:13 +0300

    made some changes

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,7 @@
 from app import app
 from flask import render_template
+from app.forms import LoginForm
+from flask import flash, redirect
 
 @app.route('/')
 @app.route('/index')
@@ -20,4 +22,10 @@ def index():
         }
 
     ]
+
     return render_template("index.html", title='houme', user=user, posts = posts)
+
+@app.route('/login', methoods=['GET', 'POST'])
+def login():
+    form = LoginForm
+    render_template('login.html', title='LogIn', form=form)
