366fbdc159a9a6e8a96eaef22f830e0b2ce221cc
Ученик <student@school509.local>
2023-03-24 17:05:53 +0300

    made some changes

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,5 @@
 from app import app
-from flask import render_template, flash, redirect
+from flask import render_template, flash, redirect, url_for
 from app.forms import LoginForm
 from flask import flash, redirect
 
@@ -29,6 +29,6 @@ def index():
 def login():
     form = LoginForm()
     if form.validate_on_submit():
-        flash(f"{form.username.data}Sign In successfully")
-        return redirect('/index')
+        flash(f"{form.username.data}  Sign In successfully")
+        return redirect(url_for('index'))
     return render_template('login.html', title='LogIn', form=form)
