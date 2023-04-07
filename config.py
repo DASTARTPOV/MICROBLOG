63046964e8d5d7cde10ab55cbc6bf293af44a000
pavst23 <p.stupnikov0723@mail.ru>
2023-04-07 16:57:11 +0300

    made some changes

--- config.py
+++ config.py
@@ -1,3 +1,6 @@
 import os
+basedir = os.path.abspath(os.path.dirname(__file__))
 class Config(object):
     SECRET_KEY = os.environ.get("SECRET_KEY") or "rs.ty4.165y.fk45.y||651fy"
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URI") or 'sqlite:///' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
