ddb9cc07e4687ce074bb2e4a142ab5fc13a72d73
Ученик <student@school509.local>
2023-05-05 17:16:00 +0300

    made some changes

--- app/models.py
+++ app/models.py
@@ -1,6 +1,8 @@
-from app import db
+from app import db, login
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin
 
-class User(db.Model):
+class User(UserMixin, db.Model):
     id = db.Column(db.Integer, prinary_key=True)
     username = db.Column(db.String(20),index=True, unique=True)
     email = db.Column(db.String(50), index=True, unique=True)
@@ -8,3 +10,14 @@ class User(db.Model):
 
     def __repr__(self):
         return  f'<User {self.username}>'
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+    @login.user_loader_
+    def load_user(id):
+        return User.query.get(int(id))
+
--- app/routes.py
+++ app/routes.py
@@ -2,6 +2,8 @@ from app import app
 from flask import render_template, flash, redirect, url_for
 from app.forms import LoginForm
 from flask import flash, redirect
+from app.models import User
+from flask_login import current_user, login_user
 
 @app.route('/')
 @app.route('/index')
@@ -27,8 +29,15 @@ def index():
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    if current_user.is_authenticated:
+        flash('You are already logged in')
+        return redirect(url_for('index'))
     form = LoginForm()
     if form.validate_on_submit():
+        user = User.query.filter_by(username=username).first()
+        if user is None or not user.check_password(form.password.data):
+            flash('Invalid username or password')
+            return redirect(url_for('login'))
         flash(f"{form.username.data}  Sign In successfully")
         return redirect(url_for('index'))
     return render_template('login.html', title='LogIn', form=form)
